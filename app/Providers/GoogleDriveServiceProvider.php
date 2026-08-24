ff08a16279214fde54a70c32c3310a4e57b34962
sultonj18@gmail.com <abcdefghijklmnopqrstuvwxyz123>
2026-08-24 14:50:52 +0700

    update

--- app/Providers/GoogleDriveServiceProvider.php
+++ app/Providers/GoogleDriveServiceProvider.php
@@ -23,23 +23,11 @@ class GoogleDriveServiceProvider extends ServiceProvider
             $client = new \Google\Client();
             $client->setClientId($config['clientId']);
             $client->setClientSecret($config['clientSecret']);
-            
-            // 1. Ambil token dan simpan responnya ke dalam variabel $token
-            $token = $client->fetchAccessTokenWithRefreshToken($config['refreshToken']);
-            
-            // 2. CEK ERROR: Jika Google menolak, hentikan aplikasi dan tampilkan alasannya!
-            if (isset($token['error'])) {
-                dd(
-                    'GAGAL MENDAPATKAN AKSES DARI GOOGLE!', 
-                    'Alasan Error: ' . $token['error'],
-                    'Deskripsi: ' . ($token['error_description'] ?? 'Tidak ada detail'),
-                    'Refresh Token yang terbaca oleh sistem: ' . $config['refreshToken']
-                );
-            }
+            $client->refreshToken($config['refreshToken']);
 
             $service = new \Google\Service\Drive($client);
             $adapter = new GoogleDriveAdapter($service, $config['folder'] ?? '/', $options);
-
+            
             $driver = new Filesystem($adapter, $config);
 
             return new FilesystemAdapter($driver, $adapter, $config);
